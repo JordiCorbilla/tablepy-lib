3de2a59412c19a90b65d0c2e09c317e9c08f374d
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2023-05-29 14:58:34 +0100

    update tests

--- tests/test_consoleFormatter.py
+++ tests/test_consoleFormatter.py
@@ -7,11 +7,10 @@
 # ---------------------------------------------------------------------------
 
 from tablepy_lib import consoleFormatter
-import pytest
-import sys
+import pandas as pd
 
 class MyFormatterTests:
-    def test_to_sql(self):
+    def test_toSql(self):
         data = {
             "Name": ["John", "Emily", "Tom", "JC"],
             "Age": [28, 32, 25, 2],
@@ -23,27 +22,24 @@ class MyFormatterTests:
         table = formatter.to_table()
         print(table)
         assert table == ""
+        
 
 
-if __name__ == "__main__":
-    sys.exit(pytest.main(["-qq"], plugins=[MyFormatterTests()]))
+data = {
+    "Name": ["John", "Emily", "Tom", "JC"],
+    "Age": [-28, 3002.6, 25, 2],
+    "Country": ["USA", "Canada", "UK", "DE"],
+    "Data": ["USA", "Canada", "UK", "3434243"]
+}
 
+formatter = consoleFormatter(data)
+table = formatter.to_table()
+print(table)    
 
-# data = {
-#     "Name": ["John", "Emily", "Tom", "JC"],
-#     "Age": [-28, 3002.6, 25, 2],
-#     "Country": ["USA", "Canada", "UK", "DE"],
-#     "Data": ["USA", "Canada", "UK", "3434243"]
-# }
+data_frame = pd.DataFrame(data)
 
-# formatter = ConsoleFormatter(data)
-# table = formatter.to_table()
-# print(table)    
+table = consoleFormatter(data_frame).to_table()
+print(table)
 
-# data_frame = pd.DataFrame(data)
-
-# table = ConsoleFormatter(data_frame).to_table()
-# print(table)
-
-# table = ConsoleFormatter(data_frame).to_sql('dd')
-# print(table)
+table = consoleFormatter(data_frame).to_sql('dd')
+print(table)
